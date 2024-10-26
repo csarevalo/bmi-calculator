4156dd7e6a485f262a800c20c85624e445367ebe
Cristian Arevalo <csarevalo1@gmail.com>
2024-10-26 15:23:03 -0700

    create getters and private vars

--- lib/src/providers/bmi_provider.dart
+++ lib/src/providers/bmi_provider.dart
@@ -2,10 +2,17 @@ import 'package:bmi_calculator/src/widgets/gender_card.dart';
 import 'package:flutter/material.dart';
 
 class BmiProvider extends ChangeNotifier {
+  // Set private BMI variables
   Gender _gender = Gender.none;
   double _height = 165; //cm
   double _weight = 130; //lb
   int _age = 30; //years
+
+  // Get BMI parameters from private variables
+  Gender get gender => _gender;
+  double get height => _height;
+  double get weight => _weight;
+  int get age => _age;
 
   /// Update user gender
   void updateGender(Gender newGender) {
